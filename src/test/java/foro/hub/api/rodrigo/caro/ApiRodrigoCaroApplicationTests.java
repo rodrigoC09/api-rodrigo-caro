6dc9c6366a66d2fa933de818a0e81c32056d60eb
rodrigoC <rodrigo.antonio.caro@gmail.com>
2024-07-10 15:53:47 -0400

    proyecto inicial

---
+++ src/test/java/foro/hub/api/rodrigo/caro/ApiRodrigoCaroApplicationTests.java
@@ -0,0 +1,13 @@
+package foro.hub.api.rodrigo.caro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRodrigoCaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
